adb12cd7b8adef7cd579576a687ae33e31d8ea21
Yevhenii Boiko <yevheniiboiko@gmail.com>
2023-01-11 14:29:39 +0100

    HW-JS-10

---
+++ src/fetchCountries.js
@@ -0,0 +1,13 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+export default function fetchCountries(name) {
+  const BASE_URL = 'https://restcountries.com/v3.1/name/';
+  return fetch(
+    `${BASE_URL}${name}?fields=name,capital,languages,population,flags,flag`
+  ).then(response => {
+    if (response.status === 404) {
+      Notify.failure('Oops, there is no country with that name');
+    }
+    return response.json();
+  });
+}
---
+++ src/index.js
@@ -0,0 +1,75 @@
+import debounce from 'lodash.debounce';
+import fetchCountries from './fetchCountries';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+import './css/styles.css';
+
+const DEBOUNCE_DELAY = 300;
+
+const input = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+input.addEventListener('input', debounce(onFetchCountries, DEBOUNCE_DELAY));
+
+function onFetchCountries(e) {
+  const { value } = e.target;
+  if (!value.trim()) {
+    deleteMarkup();
+    return;
+  }
+  fetchCountries(value.trim()).then(addMarkup);
+}
+
+function addMarkup(data) {
+  if (data.length > 1 && data.length < 10) {
+    deleteMarkup();
+    countryList.innerHTML = createListMarkup(data);
+  } else if (data.length === 1) {
+    deleteMarkup();
+    countryInfo.innerHTML = createCountryInfoMarkup(data);
+  } else if (data.length > 10) {
+    deleteMarkup();
+    Notify.info('Too many matches found. Please enter a more specific name.');
+  }
+}
+
+function createListMarkup(arr) {
+  {
+    return arr
+      .map(
+        ({ name: { common }, flags: { png } }) =>
+          `<li>
+          <h3><img src="${png}" alt="flag"> ${common}</h3>
+          </li>`
+      )
+      .join('');
+  }
+}
+
+function createCountryInfoMarkup(arr) {
+  return arr
+    .map(
+      ({
+        name: { common },
+        flags: { png },
+        capital,
+        population,
+        languages,
+      }) => {
+        return `<div>
+        
+        <h1><img src="${png}" alt="flag"> ${common}</h1>
+        <h2>Capital: <span>${capital}</span></h2>
+        <h2>Population: <span>${population}</span></h2>
+        <h2>Languages: <span>${Object.values(languages).join(', ')}</span></h2>
+    </div>`;
+      }
+    )
+    .join('');
+}
+
+function deleteMarkup() {
+  countryList.innerHTML = '';
+  countryInfo.innerHTML = '';
+}
